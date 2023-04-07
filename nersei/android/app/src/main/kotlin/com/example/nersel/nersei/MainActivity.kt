4286bf33b95ac595ffcf870a1d20c5f0dad55535
YunusEmreCinbolat <Yunus.Emre.Cinbolat@gmail.com>
2023-04-07 16:18:41 +0300

    .

--- nersei/android/app/src/main/kotlin/com/example/nersel/nersei/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.nersel.nersei
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- turk_bayragi/android/app/src/main/kotlin/com/example/turk_bayragi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.turk_bayragi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
